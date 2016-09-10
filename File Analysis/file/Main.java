d7213fec693445e14acaab9bb41dd4b45de19c5c
dfzunigah <dfzunigah@unal.edu.co>
2016-09-10 16:43:48 -0500

    Basic file analysis (lines/words)

---
+++ File Analysis/file/Main.java
@@ -0,0 +1,13 @@
+package file;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("test/testfile.txt");
+    Analysis analysis = new Analysis(file);
+    System.out.println("Lines: " + analysis.lines());
+    System.out.println("Characters: " + analysis.characters());
+    }
+}
